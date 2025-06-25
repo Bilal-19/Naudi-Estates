e2e6ec7f80c427224b8a385a64e5af4312d54b92
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-06-25 14:55:54 +0500

    Added banner image to feedback page.

--- resources/views/Visitors/Testimonials.blade.php
+++ resources/views/Visitors/Testimonials.blade.php
@@ -1,7 +1,20 @@
-@extends("UserLayout.main")
+@extends('UserLayout.main')
 
-@section("main-section")
-@push("title-tag")
-    <title>Naudi Estate - Testimonials</title>
-@endpush
+@section('main-section')
+    @push('title-tag')
+        <title>Naudi Estate - Testimonials</title>
+    @endpush
+
+    <div class="w-full bg-cover bg-no-repeat h-56 md:h-[662px] text-white flex flex-col justify-start p-10"
+        style="background-image: url('{{ asset('assets/Feedback/banner.png') }}')">
+        <h1 class="text-2xl md:text-6xl font-medium mt-30">
+            Real Stories
+            <span class="text-[#C9A578]">Stories</span>
+            from Real
+            <span class="text-[#C9A578]">Customers</span>
+        </h1>
+        <p class="text-md md:text-[25px] font-light">
+            Read authentic experiences shared by our valued customers.
+        </p>
+    </div>
 @endsection
