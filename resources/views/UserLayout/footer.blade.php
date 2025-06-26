99002a2b8d97cc52006603153b6374de84afb382
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-06-26 12:33:46 +0500

    Updated footer.

--- resources/views/UserLayout/footer.blade.php
+++ resources/views/UserLayout/footer.blade.php
@@ -10,10 +10,10 @@
     <div class="m-5 md:m-0">
         <h4 class="text-lg md:text-[25px] font-medium">Quick Links</h4>
         <div class="flex flex-col">
-            <a href="" class="font-light text-base md:text-xl hover:text-[#C9A578]">Services</a>
-            <a href="" class="font-light text-base md:text-xl hover:text-[#C9A578]">Consultation</a>
-            <a href="" class="font-light text-base md:text-xl hover:text-[#C9A578]">Pricing</a>
-            <a href="" class="font-light text-base md:text-xl hover:text-[#C9A578]">Contact</a>
+            <a href="{{route('Service')}}" class="font-light text-base md:text-xl hover:text-[#C9A578] {{ request()->routeIs('Service') ? 'text-[#C9A578]' : 'text-white' }}">Services</a>
+            <a href="{{route('Calendar')}}" class="font-light text-base md:text-xl hover:text-[#C9A578] {{ request()->routeIs('Calendar') ? 'text-[#C9A578]' : 'text-white' }}">Consultation</a>
+            <a href="{{route('Pricing')}}" class="font-light text-base md:text-xl hover:text-[#C9A578] {{ request()->routeIs('Pricing') ? 'text-[#C9A578]' : 'text-white' }}">Pricing</a>
+            <a href="{{route('Contact')}}" class="font-light text-base md:text-xl hover:text-[#C9A578] {{ request()->routeIs('Contact') ? 'text-[#C9A578]' : 'text-white' }}">Contact</a>
         </div>
     </div>
 </div>
