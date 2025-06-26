7e2461b0e9ce545158bd7f76f08ba4802042ff6d
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-06-26 12:37:32 +0500

    Updated feedback page.

--- resources/views/Visitors/Testimonials.blade.php
+++ resources/views/Visitors/Testimonials.blade.php
@@ -5,9 +5,9 @@
         <title>Naudi Estate - Testimonials</title>
     @endpush
 
-    <div class="w-full bg-cover bg-no-repeat h-56 md:h-[662px] text-white flex flex-col justify-start p-10"
+    <div class="w-full bg-cover bg-no-repeat h-52 md:h-[662px] text-white flex flex-col justify-start p-10"
         style="background-image: url('{{ asset('assets/Feedback/banner.png') }}')">
-        <h1 class="text-2xl md:text-6xl font-medium mt-30">
+        <h1 class="text-2xl md:text-6xl font-medium md:mt-30">
             Real Stories
             <span class="text-[#C9A578]">Stories</span>
             from Real
