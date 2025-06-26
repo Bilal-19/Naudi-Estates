35915d67376c69f746746a29bfb2fe5e49459e0c
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-06-26 21:22:14 +0500

    Added anchor tag to footer logo.

--- resources/views/UserLayout/footer.blade.php
+++ resources/views/UserLayout/footer.blade.php
@@ -1,7 +1,10 @@
 {{-- Footer --}}
-<div class="w-full bg-[#040404] text-white flex flex-col md:flex-row justify-around md:items-center py-10 space-y-10 md:space-y-0">
+<div
+    class="w-full bg-[#040404] text-white flex flex-col md:flex-row justify-around md:items-center py-10 space-y-10 md:space-y-0">
     <div class="m-5 md:m-0">
-        <img src="{{ asset('assets/realestateLogo.png') }}" alt="logo" class="h-24">
+        <a href="{{ route('Home') }}">
+            <img src="{{ asset('assets/realestateLogo.png') }}" alt="logo" class="object-contain h-18 m-3">
+        </a>
     </div>
     <div class="flex flex-col font-light text-base md:text-xl m-5 md:m-0">
         <a href="mailto:propertymanagernaudi@gmail.com" class="hover:text-[#C9A578]">propertymanagernaudi@gmail.com</a>
@@ -10,9 +13,12 @@
     <div class="m-5 md:m-0">
         <h4 class="text-lg md:text-[25px] font-medium">Quick Links</h4>
         <div class="flex flex-col">
-            <a href="{{route('Service')}}" class="font-light text-base md:text-xl hover:text-[#C9A578] {{ request()->routeIs('Service') ? 'text-[#C9A578]' : 'text-white' }}">Services</a>
-            <a href="{{route('Pricing')}}" class="font-light text-base md:text-xl hover:text-[#C9A578] {{ request()->routeIs('Pricing') ? 'text-[#C9A578]' : 'text-white' }}">Pricing</a>
-            <a href="{{route('Contact')}}" class="font-light text-base md:text-xl hover:text-[#C9A578] {{ request()->routeIs('Contact') ? 'text-[#C9A578]' : 'text-white' }}">Contact</a>
+            <a href="{{ route('Service') }}"
+                class="font-light text-base md:text-xl hover:text-[#C9A578] {{ request()->routeIs('Service') ? 'text-[#C9A578]' : 'text-white' }}">Services</a>
+            <a href="{{ route('Pricing') }}"
+                class="font-light text-base md:text-xl hover:text-[#C9A578] {{ request()->routeIs('Pricing') ? 'text-[#C9A578]' : 'text-white' }}">Pricing</a>
+            <a href="{{ route('Contact') }}"
+                class="font-light text-base md:text-xl hover:text-[#C9A578] {{ request()->routeIs('Contact') ? 'text-[#C9A578]' : 'text-white' }}">Contact</a>
         </div>
     </div>
 </div>
