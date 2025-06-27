2e568e07c18b89a792b11c613b9032fca1d35369
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-06-27 22:54:09 +0500

    Updated admin dashboard UI.

--- resources/views/AdminLayout/header.blade.php
+++ resources/views/AdminLayout/header.blade.php
@@ -39,12 +39,12 @@
             class="bg-[#2d241a] text-white w-64 space-y-4 py-6 px-4 absolute md:relative z-10 top-16 left-0 md:top-0 md:flex md:flex-col transform -translate-x-full md:translate-x-0 transition-transform duration-200 ease-in-out h-min-screen">
             <nav class="space-y-3 text-sm">
                 <a href="{{route('Dashboard')}}"
-                    class="flex items-center space-x-2 px-4 py-2 {{ request()->routeIs('Dashboard') ? 'bg-[#C9A578] text-white rounded hover:bg-white hover:text-[#2d241a]' : 'text-white' }}">
+                    class="flex items-center space-x-2 px-4 py-2 {{ request()->routeIs('Dashboard') ? 'bg-[#C9A578] text-white rounded hover:bg-[#a66616] hover:text-white' : 'text-white' }}">
                     <i class="fa-solid fa-house"></i>
                     <span class="inline">Dashboard</span>
                 </a>
                 <a href="{{route('Enquiries')}}"
-                    class="flex items-center space-x-2 px-4 py-2 {{ request()->routeIs('Enquiries') ? 'bg-[#C9A578] text-white rounded hover:bg-white hover:text-[#2d241a]' : 'text-white' }}">
+                    class="flex items-center space-x-2 px-4 py-2 {{ request()->routeIs('Enquiries') ? 'bg-[#C9A578] text-white rounded hover:bg-[#a66616] hover:text-white' : 'text-white' }}">
                     <i class="fa-solid fa-house"></i>
                     <span class="inline">Enquiries</span>
                 </a>
