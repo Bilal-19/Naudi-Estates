0c7d1d050091a7afcdec27d588517655d31a22f4
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-06-27 19:15:42 +0500

    Updated admin dashboard.

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -1,4 +1,22 @@
-@extends("AdminLayout.main")
+@extends('AdminLayout.main')
 
-@section("section")
+@section('section')
+    <div class="container mx-auto p-5">
+        <div class="w-full flex flex-col md:flex-row justify-between md:space-x-5 space-y-5 md:space-y-0">
+            <div class="w-80 md:w-1/3 bg-white p-5 border-b-5 border-b-5 border-[#C9A578] rounded-xl text-center">
+                <h2 class="text-md md:text-2xl font-medium">Total Enquiries</h2>
+                <p class="text-base md:text-lg font-light"><i class="fa-solid fa-circle-question"></i> 400</p>
+            </div>
+
+            <div class="w-80 md:w-1/3 bg-white p-5 border-b-5 border-b-5 border-[#C9A578] rounded-xl text-center">
+                <h2 class="text-md md:text-2xl font-medium">Testimonials</h2>
+                <p class="text-base md:text-lg font-light"><i class="fa-solid fa-comments"></i> 20</p>
+            </div>
+
+            <div class="w-80 md:w-1/3 bg-white p-5 border-b-5 border-b-5 border-[#C9A578] rounded-xl text-center">
+                <h2 class="text-md md:text-2xl font-medium">Users</h2>
+                <p><i class="fa-solid fa-user-tie"></i> 1000</p>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/AdminLayout/header.blade.php
+++ resources/views/AdminLayout/header.blade.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Naudi Estate - Admin Dashboard</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -16,7 +15,6 @@
         href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
         rel="stylesheet">
     <link href='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/main.min.css' rel='stylesheet' />
-    @stack('title-tag')
     <style>
         body {
             font-family: "Inter", sans-serif;
